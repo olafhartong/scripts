2ff3221b92113031f28947b5e62cfb0672c2fe50
Olaf Hartong <olaf@olafhartong.nl>
2017-11-29 17:54:19 +0100

    Delete sncdb-query.sh

--- sncdb-query.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for (( SN=193; SN<=197; SN++ ))
-  do for (( A=1; A<=253; A++ ))
-     do
-     echo "Generating for 159.46.$SN.$A"
-      sncdb -I 159.46.$SN.$A --no-os | grep -v Geen >> jubit-ext-$SN.txt
-  done
-done
